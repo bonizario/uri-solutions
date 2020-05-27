2ec37ab5a519d447e27bbdb4e98da5eb1f3e7e3e
Bonizario <gabrielbonizario98@gmail.com>
2020-05-26 22:41:12 -0300

    Added algorithms 05.26.20

--- algorithms/bubble_sort.cpp
+++ algorithms/bubble_sort.cpp
@@ -8,18 +8,20 @@
     There are also merge sort O(nlogn) and counting sort O(n).
 
     It is almost never a good idea to implement a sorting algorithm,
-    instead, use the sort() function from the STL.
+    instead, use the sort() function from the STL (C++) or
+    the qsort() function (C).
 */
 
-int bubbleSort(int arr[], int size) {
-    int i, j, aux;
-    for (i = 0; i < size; i++) {
-        for (j = 0; j < size-1; j++) {
-            aux = arr[j];
-            if (aux > arr[j+1]) {
-                arr[j] = arr[j+1];
-                arr[j+1] = aux;
-            }
-        }
-    }
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void bubbleSort(int arr[], int N) {
+    int i, j;
+    for (i = 0; i < N-1; i++)
+        for (j = 0; j < N-i-1; j++)
+            if (arr[j] > arr[j+1])
+                swap(&arr[j], &arr[j+1]);
 }
