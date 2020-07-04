bf3933f90accf47ae69eac09d1451e31acc68ad5
Bonizario <gabrielbonizario98@gmail.com>
2020-07-03 23:06:50 -0300

    Added algorithms 07.03.2020

--- algorithms/dijkstra.cpp
+++ algorithms/dijkstra.cpp
@@ -16,40 +16,41 @@ int processado[MAXN]; // armazena se um vértice foi processado/fechado
 vector<pii> vizinhos[MAXN]; // lista de adjacência, first => distância, second => vértice
 
 void Dijkstra(int S) {
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= n; i++) {
         distancia[i] = INFINITO;
+        processado[i] = false;
+    }
     distancia[S] = 0;
 
     priority_queue<pii, vector<pii>, greater<pii>> pq;
     pq.push(pii(distancia[S], S));
 
     while (true) {
-        int da_vez = -1;
+        int davez = -1;
 
         while (!pq.empty()) {
             int atual = pq.top().second;
             pq.pop();
 
             if (!processado[atual]) {  // ainda não foi fechado
-                da_vez = atual;
+                davez = atual;
                 break;
             }
         }
 
-        if (da_vez == -1) break;  // todos vértices foram fechados
+        if (davez == -1) break;  // todos vértices foram fechados
 
-        processado[da_vez] = true;
+        processado[davez] = true;
 
-        for (int i = 0; i < (int)vizinhos[da_vez].size(); i++) {
-            int dist = vizinhos[da_vez][i].first;
-            int atual = vizinhos[da_vez][i].second;
+        for (int i = 0; i < (int)vizinhos[davez].size(); i++) {
+            int dist = vizinhos[davez][i].first;
+            int atual = vizinhos[davez][i].second;
 
-            if (distancia[atual] > distancia[da_vez] + dist) {
-                distancia[atual] = distancia[da_vez] + dist;
+            if (distancia[atual] > distancia[davez] + dist) {
+                distancia[atual] = distancia[davez] + dist;
                 pq.push(pii(distancia[atual], atual));
             }
         }
-
     }
 }
 
@@ -63,7 +64,7 @@ int main() {
 	for (int i = 1; i <= m; i++) {
 		int x, y, tempo;
 		cin >> x >> y >> tempo;
-		// em alguns exercícios o grafo NÃO é bidirecional!!
+		// Em alguns exercícios o grafo NÃO é bidirecional!!
 		vizinhos[x].push_back( pii(tempo, y) );
 		vizinhos[y].push_back( pii(tempo, x) );
 	}
--- algorithms/kadane.cpp
+++ algorithms/kadane.cpp
@@ -2,12 +2,11 @@
 
 using namespace std;
 
-int maxSubarraySum(int arr[], int n) {
-    int i, curr_sum = arr[0], max_sum = arr[0];
-    for (i = 1; i <= n; i++) {
+int maxSubarraySum(int n, int arr[]) {
+    int curr_sum = arr[0], max_sum = arr[0];
+    for (int i = 1; i < n; i++) {
         curr_sum = max(arr[i], curr_sum + arr[i]);
         max_sum = max(curr_sum, max_sum);
     }
-
     return max_sum;
 }
